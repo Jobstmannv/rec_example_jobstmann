78bc19d97682318a0511538b4f17a0b71958cf81
Jobstmannv <jobstmannv@gmail.com>
2023-04-10 18:38:48 +0200

    Readme updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3e6650838eaf96d4"; }
+/******/ 		__webpack_require__.h = function() { return "15a20619d1f6bbee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.dd0529713d1106e7.hot-update.js
+++ .next/static/webpack/webpack.dd0529713d1106e7.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "3e6650838eaf96d4"; }
+/******/ 	__webpack_require__.h = function() { return "d17d8cbc4e5d9359"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
